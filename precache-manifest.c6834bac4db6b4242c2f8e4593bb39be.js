389962c1493571e2834d4abfd0cf60c0c66794fe
libra1919 <aymentli@gmail.com>
2020-10-06 03:03:15 +0200

    Updates

---
+++ precache-manifest.c6834bac4db6b4242c2f8e4593bb39be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526eca2f067c487528d135fec6701d5b",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "ef330d0df1e63f461048",
+    "url": "/movies/static/css/main.de51cac3.chunk.css"
+  },
+  {
+    "revision": "dac431f277d7715d2493",
+    "url": "/movies/static/js/2.60550c8e.chunk.js"
+  },
+  {
+    "revision": "1f83015dc635997a697ee225e2f4c347",
+    "url": "/movies/static/js/2.60550c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef330d0df1e63f461048",
+    "url": "/movies/static/js/main.71de508f.chunk.js"
+  },
+  {
+    "revision": "45f1b12226fb8957f395",
+    "url": "/movies/static/js/runtime-main.6ee545b8.js"
+  }
+]);
