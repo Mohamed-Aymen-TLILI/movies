8cd1289680d323744e2170cabdbf13f91f647511
Mohamed Aymen TLILI <45849423+Mohamed-Aymen-TLILI@users.noreply.github.com>
2020-10-06 03:05:39 +0200

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,14 +5,14 @@ export default class Header extends Component {
   render() {
     return (
       <header className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/movies/film">Films</a>
+        <a className="navbar-brand" href="/movies">Films</a>
         <button className="navbar-toggler">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink to="/movies/film" className="nav-link" activeClassName="active" > Films </NavLink>
+              <NavLink to="/movies" className="nav-link" activeClassName="active" > Films </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="/movies/favoris" className="nav-link" activeClassName="active"> Favoris </NavLink>
